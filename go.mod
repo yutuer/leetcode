d1acfcc42d5328bd9d0fadc083a34aa2f5dafd17
yutuer <yutuerzf@163.com>
2019-06-10 21:29:37 +0800

    11

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/yutuer/leetcode
 
 go 1.12
 
-require github.com/pkg/errors v0.8.1
+require github.com/yutuer/mydatastruct v0.0.2
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+import "github.com/yutuer/mydatastruct/stack"
 
 func main() {
 	fmt.Println("test")
+
+	myStack := stack.NewStack()
+	myStack.Push(1)
+
+	i, _ := myStack.Pop()
+
+	fmt.Println(i)
 }
