4afcf2cbd17a08b64899ea1170f55e8d686e582f
yutuer <yutuerzf@163.com>
2019-07-15 12:23:29 +0800

    1:

--- mybolt/db.go
+++ mybolt/db.go
@@ -1,14 +1,75 @@
 package mybolt
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 var defaultPageSize = os.Getpagesize()
 
+// DefaultOptions represent the options used if nil options are passed into Open().
+// No timeout is used which will cause Bolt to wait indefinitely for a lock.
+var DefaultOptions = &Options{
+	Timeout:    0,
+	NoGrowSync: false,
+}
+
 type DB struct {
+
+	// When true, skips the truncate call when growing the database.
+	// Setting this to true is only safe on non-ext3/ext4 systems.
+	// Skipping truncation avoids preallocation of hard drive space and
+	// bypasses a truncate() and fsync() syscall on remapping.
+	//
+	// https://github.com/boltdb/bolt/issues/284
+	NoGrowSync bool
+
+	// If you want to read the entire database fast, you can set MmapFlag to
+	// syscall.MAP_POPULATE on Linux 2.6.23+ for sequential read-ahead.
+	MmapFlags int
+
+	opened bool
 
 }
 
-func (db *DB) String() string{
+// Open creates and opens a database at the given path.
+// If the file does not exist then it will be created automatically.
+// Passing in nil options will cause Bolt to open the database with the default options.
+func Open(path string, mode os.FileMode, options *Options) (*DB, error) {
+	db := &DB{opened: true}
+
+	// Set default options if no options are provided.
+	if options == nil {
+		options = DefaultOptions
+	}
+	db.NoGrowSync = options.NoGrowSync
+	db.MmapFlags = options.MmapFlags
+
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, mode)
+	if err != nil {
+		return nil, err
+	}
+
+
+
+	return db, err
+}
+
+func (db *DB) String() string {
 	return ""
 }
 
+func (db *DB) Close() error {
+
+}
+
+// Options represents the options that can be set when opening a database.
+type Options struct {
+	// Timeout is the amount of time to wait to obtain a file lock.
+	// When set to zero it will wait indefinitely. This option is only
+	// available on Darwin and Linux.
+	Timeout time.Duration
+	// Sets the DB.NoGrowSync flag before memory mapping the file.
+	NoGrowSync bool
+}
