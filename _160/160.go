7ec7435d6ec1638445ab043d220aa351fbd36920
yutuer <yutuerzf@163.com>
2020-07-23 16:01:46 +0800

    111

--- _160/160.go
+++ _160/160.go
@@ -1,10 +1,31 @@
 package _160
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
 
+	var bOver, aOver = false, false
+	curA, curB := headA, headB
 
+	for ; curA != curB && curA != nil && curB != nil; {
+		curA = curA.Next
+		if curA == nil {
+			if !bOver {
+				curA = headB
+				bOver = true
+			}
+		}
 
-	return nil
+		curB = curB.Next
+		if curB == nil {
+			if !aOver {
+				curB = headA
+				aOver = true
+			}
+		}
+	}
+	return curA
 }
 
 type ListNode struct {
